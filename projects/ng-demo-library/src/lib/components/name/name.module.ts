fdd1da49060556e03f2d67e592c35810467c987c
devisfun84 <devisfun84@gmail.com>
2020-08-23 12:11:08 +0530

    initial modules set up for name and time reusable components

---
+++ projects/ng-demo-library/src/lib/components/name/name.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NameComponent } from './name.component';
+
+
+
+@NgModule({
+  declarations: [NameComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NameModule { }
